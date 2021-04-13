254f0e14722f42cd97f2a4f6baa8c02d4d088434
Dantoss <82037854+Dantoss@users.noreply.github.com>
2021-04-13 20:32:31 +0700

    Добавлен второй игрок

--- ping_pong.py
+++ ping_pong.py
@@ -14,10 +14,14 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_left(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 0: self.rect.y -= self.speed
         if keys_pressed[K_s] and self.rect.y < 375: self.rect.y += self.speed
+    def update_right(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_UP] and self.rect.y > 0: self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 375: self.rect.y += self.speed
 
 
 
@@ -35,6 +39,7 @@ window = display.set_mode((700, 500))
 display.set_caption("Шутер")
 background = transform.scale(image.load("galaxy.jpg"), (700, 500))
 player = Player('racket.jpeg',0, 200, 50, 120, 15)
+player2 = Player('racket.jpeg',500, 200, 50, 120, 15)
 
 font.init()
 font1 = font.SysFont('Arial', 36)
@@ -47,7 +52,9 @@ while game:
     if finish != True:
         window.blit(background, (0, 0))
         player.reset()
-        player.update()
+        player.update_left()
+        player2.reset()
+        player2.update_right()
         clock.tick(FPS)
         
         # lost_text = font1.render(f'Пропущено: {lost}', 1, (255,255,255))
